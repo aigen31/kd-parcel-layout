d57f11a0ad66206fa7926a0177733225932cb08b
Evgeny Bil <aigen.chernogorsk@yandex.ru>
2024-07-04 00:12:36 +0700

    Add new sections

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
-import Alpine from "alpinejs";
+import Alpine from "alpinejs"
+import Glide from "@glidejs/glide"
 
 window.Alpine = Alpine
 
-Alpine.start();
+Alpine.start()
+
+let glide = new Glide('.glide', {
+  type: 'carousel',
+  perView: 4,
+  gap: 0,
+  breakpoints: {
+    1024: {
+      perView: 3
+    },
+    768: {
+      perView: 2
+    },
+    640: {
+      perView: 1
+    }
+  }
+})
+
+glide.mount()
